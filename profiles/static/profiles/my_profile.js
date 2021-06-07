15774aae88531793aed906b5b0ed67efd1cb7602
KAA-sign <q89308033587@gmail.com>
2021-06-07 16:34:32 +0300

    spinn

--- profiles/static/profiles/my_profile.js
+++ profiles/static/profiles/my_profile.js
@@ -1,1 +1,11 @@
 console.log('hello world')
+
+const toFollowModalBody = document.getElementById('to-follow-modal-body') 
+const spinnerBox = document.getElementById('spinner-box')
+
+console.log(toFollowModalBody)
+console.log(spinnerBox)
+
+$.ajax({
+
+})
