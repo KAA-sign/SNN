1221c3829b38d44a700309f5d80d6fa7fa6f74dc
KAA-sign <q89308033587@gmail.com>
2021-06-09 14:44:43 +0300

    follow list

--- profiles/static/profiles/my_profile.js
+++ profiles/static/profiles/my_profile.js
@@ -1,20 +1,44 @@
 console.log('hello world')
 
-const toFollowModalBody = document.getElementById('to-follow-modal-body') 
+const toFollowModalBody = document.getElementById('to-follow-modal-body')
+const toFollowBtn = document.getElementById('to-follow-btn')
 const spinnerBox = document.getElementById('spinner-box')
+let toFollowLoaded = false
 
 console.log(toFollowModalBody)
 console.log(spinnerBox)
 
-$.ajax({
-    type: 'GET',
-    url: '/profiles/my-profile-json/',
-    success: function(response){
-        console.log(response)
-    },
-    error: function(error){
-        console.log(error)
-    }
-
-
+toFollowBtn.addEventListener('click', () => {
+    $.ajax({
+        type: 'GET',
+        url: '/profiles/my-profile-json/',
+        success: function(response){
+            if(!toFollowLoaded){
+                const pfData = response.pf_data
+                console.log(pfData)
+                setTimeout(() =>{
+                    spinnerBox.classList.add('not-visible')
+                    pfData.forEach(el=>{
+                        toFollowModalBody.innerHTML += `
+                            <div class="row mb-2 align-items-center">
+                                <div class="col-2">
+                                    <img class="avatar" src="${el.avatar}" alt="${el.user}">
+                                </div>
+                                <div class="col-3">
+                                    <div class="text-muted">${el.user}</dev>
+                                </div>
+                                <div class="col text-right">
+                                    <button class="btn btn-success">follow</button>
+                                </div>
+                            </div>
+                        `
+                    })
+                }, 2000)
+            }
+            toFollowLoaded = true
+        },
+        error: function(error){
+            console.log(error)
+        }
+    })
 })
